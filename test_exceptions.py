6d1463d28678128c0641d59db30ebaa3b3f01c19
Julian Heinovski <julian.heinovski@ccs-labs.org>
2016-10-11 17:55:09 +0200

    Add file for testing more exceptions

---
+++ test_exceptions.py
@@ -0,0 +1,13 @@
+import pytest
+
+def test_zero_division():
+    with pytest.raises(ZeroDivisionError, message="Expecting ZeroDivisionError"):
+        1 / 0
+
+
+def test_recursion_depth():
+    with pytest.raises(RuntimeError) as excinfo:
+        def f():
+            f()
+        f()
+    assert 'maximum recursion' in str(excinfo.value)
